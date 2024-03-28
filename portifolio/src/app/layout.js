1d4640a8814a52f3fdd6efbf5226f0a62b5dd5cf
Bernardo <bernardo.geus@pucpr.edu.br>
2024-03-27 23:27:33 -0300

    adding Vercel's Analitcs and Spped Insights

--- portifolio/src/app/layout.js
+++ portifolio/src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,7 +25,11 @@ export default function RootLayout({ children }) {
           href="https://cdn.jsdelivr.net/gh/dheereshagrwal/colored-icons@1.7.5/src/app/ci.min.css"
         />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body 
+      className={inter.className}>{children}        
+      <SpeedInsights />
+      <Analytics />
+      </body>
     </html>
   );
 }
